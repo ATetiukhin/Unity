3219f32abe80018770cd2e639f3f967229a5aeda
Artyom Tetiukhin <artemtetiukhin@yandex.com>
2017-02-20 18:16:03 +0300

    Initial commit

---
+++ Assets/sphere_1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphere_1 : MonoBehaviour
+{
+
+    float t = 0;
+    float x = 0;
+    float y = 0;
+    float z = 0;
+
+    // Use this for initialization
+    void Start() {
+        x = transform.position.x;
+        z = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        t = Time.deltaTime;
+        y = 5 * Mathf.Sin(t);
+        transform.position = new Vector3(x, y, z);
+    }
+}
